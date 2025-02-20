5af916e5dc53f1e9d5bdb18b848efa399b495dc8
Ronit9320 <shahronit16@gmail.com>
2025-02-20 19:27:20 +0530

    utils

---
+++ src/file_io/shader_utils.cpp
@@ -0,0 +1,118 @@
+#include "shader_utils.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+namespace ShaderUtils {
+
+std::string loadShaderFromFile(const std::string &filepath) {
+  std::ifstream file(filepath);
+  if (!file.is_open()) {
+    std::cerr << "Failed to open shader file: " << filepath << std::endl;
+    return "";
+  }
+
+  // Read file into string using string stream
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+GLuint compileShader(GLenum shaderType, const std::string &source) {
+  GLuint shader = glCreateShader(shaderType);
+  const char *sourcePtr = source.c_str();
+  glShaderSource(shader, 1, &sourcePtr, nullptr);
+  glCompileShader(shader);
+
+  if (!checkShaderErrors(shader, GL_COMPILE_STATUS)) {
+    std::cerr << "Shader compilation failed!" << std::endl;
+    printShaderLog(shader);
+    glDeleteShader(shader);
+    return 0;
+  }
+
+  return shader;
+}
+
+GLuint createShaderProgram(const std::string &vertexShaderPath,
+                           const std::string &fragmentShaderPath) {
+  // Load shader sources
+  std::string vertexSource = loadShaderFromFile(vertexShaderPath);
+  std::string fragmentSource = loadShaderFromFile(fragmentShaderPath);
+
+  if (vertexSource.empty() || fragmentSource.empty()) {
+    std::cerr << "Failed to load shader sources" << std::endl;
+    return 0;
+  }
+
+  // Compile shaders
+  GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+  GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+  if (vertexShader == 0 || fragmentShader == 0) {
+    if (vertexShader != 0)
+      glDeleteShader(vertexShader);
+    if (fragmentShader != 0)
+      glDeleteShader(fragmentShader);
+    return 0;
+  }
+
+  // Create and link program
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+
+  // Check linking status
+  if (!checkShaderErrors(program, GL_LINK_STATUS)) {
+    std::cerr << "Shader program linking failed!" << std::endl;
+    printProgramLog(program);
+    glDeleteProgram(program);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return 0;
+  }
+
+  // Cleanup shaders
+  glDetachShader(program, vertexShader);
+  glDetachShader(program, fragmentShader);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  return program;
+}
+
+bool checkShaderErrors(GLuint object, GLenum statusType) {
+  GLint success = 0;
+  if (statusType == GL_COMPILE_STATUS) {
+    glGetShaderiv(object, statusType, &success);
+  } else if (statusType == GL_LINK_STATUS) {
+    glGetProgramiv(object, statusType, &success);
+  }
+  return success != GL_FALSE;
+}
+
+void printShaderLog(GLuint shaderObj) {
+  GLint logLength = 0;
+  glGetShaderiv(shaderObj, GL_INFO_LOG_LENGTH, &logLength);
+
+  if (logLength > 0) {
+    std::vector<char> log(logLength);
+    glGetShaderInfoLog(shaderObj, logLength, &logLength, log.data());
+    std::cerr << "Shader log: " << log.data() << std::endl;
+  }
+}
+
+void printProgramLog(GLuint program) {
+  GLint logLength = 0;
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+
+  if (logLength > 0) {
+    std::vector<char> log(logLength);
+    glGetProgramInfoLog(program, logLength, &logLength, log.data());
+    std::cerr << "Program log: " << log.data() << std::endl;
+  }
+}
+
+} // namespace ShaderUtils
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
+#include "../src/file_io/shader_utils.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
-#include <fstream>
 #include <iostream>
-#include <sstream>
+#include <ostream>
 #include <string>
 
 const size_t buffer_width = 640;
@@ -19,34 +20,28 @@ struct Buffer {
 void error_callback(int error, const char *description);
 uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b);
 void buffer_clear(Buffer *buffer, uint32_t color);
+void setup_texture(GLuint *texture);
+void setup_quad(GLuint *vao, GLuint *vbo);
 
 int main() {
-
-  std::ifstream inFile("../shaders/basic.frag");
-  if (!inFile) {
-    std::cerr << "Couldn't open file" << std::endl;
-    return -1;
-  } else {
-    std::cout << "File opened successfully" << std::endl;
-  }
-
   glfwSetErrorCallback(error_callback);
   GLFWwindow *window;
 
   if (!glfwInit()) {
     return -1;
-  };
+  }
 
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
+  window = glfwCreateWindow(buffer_width, buffer_height, "Space Invaders", NULL,
+                            NULL);
   if (!window) {
     glfwTerminate();
     return -1;
-  };
+  }
   glfwMakeContextCurrent(window);
 
   GLenum err = glewInit();
@@ -59,9 +54,30 @@ int main() {
   int glVersion[2] = {-1, 1};
   glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
   glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
-
   printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
 
+  // Create and setup texture
+  GLuint texture;
+  setup_texture(&texture);
+
+  // Create and setup vertex data
+  GLuint vao, vbo;
+  setup_quad(&vao, &vbo);
+
+  // Load and compile shaders
+  std::string shaderDir = "shaders/";
+  std::string vertexShaderPath = shaderDir + "basic.vert";
+  std::string fragmentShaderPath = shaderDir + "basic.frag";
+
+  GLuint shaderProgram =
+      ShaderUtils::createShaderProgram(vertexShaderPath, fragmentShaderPath);
+  if (shaderProgram == 0) {
+    std::cerr << "Failed to create shader program" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Create and clear buffer
   uint32_t clear_color = rgb_to_uint32(0, 128, 0);
   Buffer buffer;
   buffer.width = buffer_width;
@@ -69,17 +85,81 @@ int main() {
   buffer.data = new uint32_t[buffer.width * buffer.height];
   buffer_clear(&buffer, clear_color);
 
-  glClearColor(1.0, 0.0, 0.0, 1.0);
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwSwapBuffers(window);
+    // Update texture with buffer data
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, buffer.width, buffer.height,
+                    GL_RGBA, GL_UNSIGNED_BYTE, buffer.data);
 
+    // Render quad with texture
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // 6 vertices for 2 triangles
+
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  // Cleanup
+  delete[] buffer.data;
+  glDeleteProgram(shaderProgram);
+  glDeleteVertexArrays(1, &vao);
+  glDeleteBuffers(1, &vbo);
+  glDeleteTextures(1, &texture);
   glfwDestroyWindow(window);
   glfwTerminate();
-};
+
+  return 0;
+}
+
+void setup_texture(GLuint *texture) {
+  glGenTextures(1, texture);
+  glBindTexture(GL_TEXTURE_2D, *texture);
+
+  // Initialize empty texture
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, buffer_width, buffer_height, 0,
+               GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+  // Set texture parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
+void setup_quad(GLuint *vao, GLuint *vbo) {
+  // Vertex data for a fullscreen quad (2 triangles)
+  float vertices[] = {
+      // positions   // texture coords
+      -1.0f, -1.0f, 0.0f, 0.0f, // bottom left
+      1.0f,  -1.0f, 1.0f, 0.0f, // bottom right
+      1.0f,  1.0f,  1.0f, 1.0f, // top right
+      -1.0f, -1.0f, 0.0f, 0.0f, // bottom left
+      1.0f,  1.0f,  1.0f, 1.0f, // top right
+      -1.0f, 1.0f,  0.0f, 1.0f  // top left
+  };
+
+  glGenVertexArrays(1, vao);
+  glGenBuffers(1, vbo);
+
+  glBindVertexArray(*vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Position attribute
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Texture coord attribute
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                        (void *)(2 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+}
 
 void error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
