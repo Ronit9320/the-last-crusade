b0092e21e72d1e531c21851cf2a5693fc1f99bc4
Ronit9320 <shahronit16@gmail.com>
2025-04-08 22:11:20 +0530

    fix

---
+++ src/GLUtils/window.cpp
@@ -0,0 +1,21 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void createWindow() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "START", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glewInit();
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,5 @@
 int main() {
   std::cout << "starting window creation" << std::endl;
   createWindow();
+  return 0;
 }
